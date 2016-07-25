aa9ab7cdc3c33c1eb5ee3920989ca8bcca5c84bd
Adam (Mac) <scoggins.aj@gmail.com>
2016-07-25 17:41:57 -0500

    Initial commit

--- My-Hood/ViewController.swift
+++ My-Hood/ViewController.swift
@@ -2,24 +2,58 @@
 //  ViewController.swift
 //  My-Hood
 //
-//  Created by Mac Owner on 7/25/16.
-//  Copyright © 2016 Mac Owner. All rights reserved.
+//  Created by Adam on 7/25/16.
+//  Copyright © 2016 Adam. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Below sources absolutely required
+        tableView.delegate = self
+        tableView.dataSource = self
+        DataService.instance.loadPosts()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.onPostsLoaded(_:)), name: "postsLoaded", object: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = DataService.instance.loadedPosts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.loadedPosts.count
+    }
+    
+    func onPostsLoaded(notif: AnyObject) {
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
